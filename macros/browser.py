dbbc8aa1f4abd6b2c459684365f4ab254667d439
Andy Piper <andypiper@users.noreply.github.com>
2024-05-21 22:08:12 +0100

    Updated to v9.
    Signed-off-by: Andy Piper <andypiper@users.noreply.github.com>

---
+++ macros/browser.py
@@ -0,0 +1,31 @@
+# MACROPAD Hotkeys example: Safari and Chrome for Mac
+
+from adafruit_hid.keycode import Keycode  # REQUIRED if using Keycode.* values
+
+app = {                    # REQUIRED dict, must be named 'app'
+    'name': 'Browser',  # Application name
+    'macros': [           # List of button macros...
+        # COLOR    LABEL    KEY SEQUENCE
+        # 1st row ----------
+        (0x004000, '< Back', [Keycode.COMMAND, '[']),
+        (0x004000, 'Fwd >', [Keycode.COMMAND, ']']),
+        (0x400000, 'Up', [Keycode.SHIFT, ' ']),      # Scroll up
+        # 2nd row ----------
+        (0x202000, '< Tab', [Keycode.CONTROL, Keycode.SHIFT, Keycode.TAB]),
+        (0x202000, 'Tab >', [Keycode.CONTROL, Keycode.TAB]),
+        (0x400000, 'Down', ' '),                     # Scroll down
+        # 3rd row ----------
+        (0x000040, 'Reload', [Keycode.COMMAND, 'r']),
+        (0x000040, 'Home', [Keycode.COMMAND, 'H']),
+        (0x000040, 'Private', [Keycode.COMMAND, 'N']),
+        # 4th row ----------
+        (0x000000, 'GH', [Keycode.COMMAND, 'n', -Keycode.COMMAND,
+                           'github.com\n']),   # Adafruit in new window
+        (0x800000, 'Masto', [Keycode.COMMAND, 'n', -Keycode.COMMAND,
+                            'macaw.social/home\n']),   # Digi-Key in new window
+        (0x101010, 'HAD', [Keycode.COMMAND, 'n', -Keycode.COMMAND,
+                             'hackaday.com\n']),  # Hack-a-Day in new win
+        # Encoder button ---
+        (0x000000, '', [Keycode.COMMAND, 'w'])  # Close window/tab
+    ]
+}
